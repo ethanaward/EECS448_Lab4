ec0da9e04b60e37463431de20234162ff774173f
Ethan Ward <ethanandrewward@gmail.com>
2016-02-28 17:44:22 -0600

    Update Exercise 2

---
+++ Quiz.php
@@ -0,0 +1,16 @@
+<?php
+
+$answer1 = $_POST["Question1"];
+$answer2 = $_POST["Question2"];
+$answer3 = $_POST["Question3"];
+$answer4 = $_POST["Question4"];
+$answer5 = $_POST["Question5"];
+
+echo "<p>Your first answer was: " . $answer1 . "<br></p>";
+echo "<p>Your second answer was: " . $answer2 . "<br></p>";
+echo "<p>Your third answer was: " . $answer3 . "<br></p>";
+echo "<p>Your fourth answer was: " . $answer4 . "<br></p>";
+echo "<p>Your fifth answer was: " . $answer5 . "<br></p>";
+
+
+?>
